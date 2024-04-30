970d74085ea79ba5b66a25eeb977ff67a302ed58
William Anaya <william.anaya@globant.com>
2024-04-30 17:58:10 -0500

    william.anaya-091

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function countingValleys() 	{
+function countingValleys(steps) 	{
 
+        let mountain = 0;
+        let valley = 0;
+      
+        for (let step of steps) {
+            console.log(step)
+          if (step === 'U') {
+            mountain++;
+            if (mountain === 0) {
+                valley++;
+            }
+          } else if (step === 'D') {
+            mountain--;
+          }
+        }
+      
+        console.log('mountain', mountain);
+        console.log('valley', valley);
+        return valley;
+ 
 }
+
+  const input = ['D', 'D', 'U', 'U', 'U', 'U', 'D', 'D'];
+  console.log( countingValleys(input));
